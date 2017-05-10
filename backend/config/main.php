aad10094f81579a57549cf104cd8e2e0d29368d3
Maria Benedict Nishanth A <nisha.com126@gmail.com>
2017-05-10 11:07:12 +0530

    Enabling Pretty URL & DB Configuration

--- backend/config/main.php
+++ backend/config/main.php
@@ -23,7 +23,7 @@ return [
         ],
         'session' => [
             // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
+            'name' => 'TALOZO-backend',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -37,14 +37,14 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
