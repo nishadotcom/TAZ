7defb7a29de8e29f5dab6b28f1dbcf5b1c0251e7
pradeep <apradeep.anthony@gmail.com>
2017-06-23 23:56:52 +0530

    demo controller

--- frontend/controllers/DemoController.php
+++ frontend/controllers/DemoController.php
@@ -35,7 +35,65 @@ class DemoController extends Controller
     }
     public function actionProfiledashboard()
     {
+        $this->layout = 'profile_page';
         return $this->render('profiledashboard');
     }
+    public function actionAccountprofile()
+    {
+         $this->layout = 'profile_page';
+        return $this->render('account_profile');
+    }
+    public function actionMyAddress()
+    {
+         $this->layout = 'profile_page';
+        return $this->render('my_address');
+    }
+    public function actionAllOrders()
+    {
+         $this->layout = 'profile_page';
+        return $this->render('all_orders');
+    }
+    public function actionWishList()
+    {
+         $this->layout = 'profile_page';
+        return $this->render('all_orders');
+    }
+    public function actionSingleOrder()
+    {
+        $this->layout = 'profile_page';
+        return $this->render('single_order');
+    }
+    public function actionSignup()
+    {
+          return $this->render('signup');
+    }
+    public function actionCart()
+    {      $this->layout = 'cart_page';
+          return $this->render('cart');
+    }
+    public function actionCheckout()
+    {      
+          return $this->render('checkout');
+    }
+    public function actionCheckoutStep2()
+    {      
+          return $this->render('checkout_step2');
+    }
+    public function actionCheckoutStep3()
+    {      
+          return $this->render('checkout_step3');
+    }
+    public function actionCheckoutStep4()
+    {      
+          return $this->render('checkout_step4');
+    }
+     public function actionCheckoutConfirm()
+    {      
+          return $this->render('checkout_confirm');
+    }
+
+
+    
+    
     
 }
