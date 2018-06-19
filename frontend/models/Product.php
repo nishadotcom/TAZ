937c7ea09a4c2bb54ff3cf1b90b52c164659a66c
Maria Benedict Nishanth A <nisha.com126@gmail.com>
2018-06-19 22:00:09 +0530

    Fixing Product Add Issues

--- frontend/models/Product.php
+++ frontend/models/Product.php
@@ -56,11 +56,13 @@ class Product extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['product_category_id', 'product_code', 'product_name', 'product_seo', 'product_owner_id', 'product_material', 'product_color', 'created_on'], 'required'],
+            [['product_category_id', 'product_code', 'product_name', 'product_seo', 'product_owner_id', 'product_material', 'product_color', 'created_on', 'product_price', 'product_breadth', 'product_weight', 'product_long_description'], 'required'],
             [['product_long_description'], 'required','on'=>'admin_validate'],
             [['product_category_id', 'product_subcategory_id', 'product_owner_id'], 'integer'],
             [['product_code', 'product_name', 'product_seo', 'product_dimension_type', 'product_short_description', 'product_long_description', 'product_discount_status', 'product_guarantee_status', 'product_status'], 'string'],
             [['product_price', 'product_sale_price', 'product_retail_price', 'product_height', 'product_length', 'product_breadth', 'product_weight'], 'number'],
+            [['product_height'], 'default', 'value'=>0.00],
+            [['product_length'], 'default', 'value'=>0.00],
             [['created_on', 'updated_on', 'admin_note'], 'safe'],
             [['product_material'], 'string', 'max' => 600],
             [['product_color'], 'string', 'max' => 30],
