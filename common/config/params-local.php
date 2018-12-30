6cde103c00f422cda9a7e6d5477dfeace7c9cf6a
jackvijayakanth <vijaylg@gmail.com>
2018-12-30 14:37:56 +0530

    fonts

--- common/config/params-local.php
+++ common/config/params-local.php
@@ -21,15 +21,15 @@ if($serverName=='dev.talozo.local'){
     return [
         'PROFILE_IMAGE_UPLOAD_PATH' => \Yii::getAlias('@common') . '/uploads/profile_images/',
         'PROFILE_IMAGE_UPLOAD_PATH_FRONTEND' => 'common/uploads/profile_images/',
-        'PATH_PROFILE_IMAGE' => '/dev/common/uploads/profile_images/',
+        'PATH_PROFILE_IMAGE' => 'common/uploads/profile_images/',
         'SLIDER_IMAGE_UPLOAD_PATH' => \Yii::getAlias('@common') . '/uploads/banner/',
         'SLIDER_IMAGE_DISPLAY_PATH_BACKEND' => '../common/uploads/banner/',
-        'PATH_BANER_IMAGE' => '/dev/common/uploads/banner/',
+        'PATH_BANER_IMAGE' => 'common/uploads/banner/',
         'CATEGORY_IMAGE_UPLOAD_PATH' => \Yii::getAlias('@common') . '/uploads/category_images/',
         'CATEGORY_IMAGE_DISPLAY_PATH_BACKEND' => '../common/uploads/category_images/',
         'CATEGORY_IMAGE_DISPLAY_PATH_FRONTEND' => 'common/uploads/category_images/',
         'PATH_UPLOAD_PRODUCT_IMAGE' => \Yii::getAlias('@common') . '/uploads/product_images/',
-        'PATH_PRODUCT_IMAGE' => '/dev/common/uploads/product_images/',
+        'PATH_PRODUCT_IMAGE' => 'common/uploads/product_images/',
     ];
 }
 
