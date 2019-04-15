df4eb9ad4072fbfda2350ec97aee518a8978955c
nishadotcom <nisha.com126@gmail.com>
2019-04-15 12:31:26 -0700

    Path configuration modified

--- common/config/params-local.php
+++ common/config/params-local.php
@@ -2,7 +2,7 @@
 //echo $serverName;
 $linux_user = exec('who');
 //if ($linux_user && (strpos($linux_user, 'nisha') || (strpos($linux_user, 'nisha') == 0))) {
-if($serverName=='dev.talozo.local'){
+if($serverName != 'talozo.com'){
     return [
         'PROFILE_IMAGE_UPLOAD_PATH' => \Yii::getAlias('@common') . '/uploads/profile_images/',
         'PROFILE_IMAGE_UPLOAD_PATH_FRONTEND' => 'common/uploads/profile_images/',
@@ -20,16 +20,16 @@ if($serverName=='dev.talozo.local'){
     // Online
     return [
         'PROFILE_IMAGE_UPLOAD_PATH' => \Yii::getAlias('@common') . '/uploads/profile_images/',
-        'PROFILE_IMAGE_UPLOAD_PATH_FRONTEND' => 'common/uploads/profile_images/',
-        'PATH_PROFILE_IMAGE' => 'common/uploads/profile_images/',
+        'PROFILE_IMAGE_UPLOAD_PATH_FRONTEND' => '/dev/common/uploads/profile_images/',
+        'PATH_PROFILE_IMAGE' => '/dev/common/uploads/profile_images/',
         'SLIDER_IMAGE_UPLOAD_PATH' => \Yii::getAlias('@common') . '/uploads/banner/',
         'SLIDER_IMAGE_DISPLAY_PATH_BACKEND' => '../common/uploads/banner/',
-        'PATH_BANER_IMAGE' => 'common/uploads/banner/',
+        'PATH_BANER_IMAGE' => '/dev/common/uploads/banner/',
         'CATEGORY_IMAGE_UPLOAD_PATH' => \Yii::getAlias('@common') . '/uploads/category_images/',
         'CATEGORY_IMAGE_DISPLAY_PATH_BACKEND' => '../common/uploads/category_images/',
-        'CATEGORY_IMAGE_DISPLAY_PATH_FRONTEND' => 'common/uploads/category_images/',
+        'CATEGORY_IMAGE_DISPLAY_PATH_FRONTEND' => '/dev/common/uploads/category_images/',
         'PATH_UPLOAD_PRODUCT_IMAGE' => \Yii::getAlias('@common') . '/uploads/product_images/',
-        'PATH_PRODUCT_IMAGE' => 'common/uploads/product_images/',
+        'PATH_PRODUCT_IMAGE' => '/dev/common/uploads/product_images/',
     ];
 }
 
